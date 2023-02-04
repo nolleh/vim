a77d37c737b838e5438e90684525ff67f68110a0
nolleh7707 <nolleh7707@gmail.com>
2023-02-05 00:07:28 +0900

    feat(nvim): add goto with split

--- lua-nvim/mappings.lua
+++ lua-nvim/mappings.lua
@@ -6,6 +6,16 @@ M.general = {
     ["<leader><leader>r"] = { ":Jaq<CR>" },
     ["<leader>.z"] = { ":ZoomToggle<CR>" },
     ["<leader>mm"] = { ":MinimapToggle<CR>" },
+
+    -- lsp
+    ["gv"] = {
+      ":vsplit | lua vim.lsp.buf.definition()<CR>",
+      "vertial split lsp definition",
+    },
+    ["gs"] = {
+      ":split | lua vim.lsp.buf.definition()<CR>",
+      "split lsp definition",
+    },
   },
   v = {
     ["<leader><leader>r"] = { ":Jaq<CR>" },
