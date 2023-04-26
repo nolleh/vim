456757a74cf716a26d2b052b374494e0f20156c6
nolleh7707 <nolleh7707@gmail.com>
2023-04-27 00:22:38 +0900

    feat(vim): plugin load trigger

--- lua-nvim/configs/init.lua
+++ lua-nvim/configs/init.lua
@@ -1,94 +1,65 @@
 -- return {}
 return {
-	{
-		"nvim-tree/nvim-tree.lua",
-		opts = {
-			renderer = {
-				highlight_git = true,
-			},
-			view = {
-				width = {
-					max = 40,
-				},
-			},
-		},
-	},
+    {
+        "nvim-tree/nvim-tree.lua",
+        opts = {
+            renderer = {
+                highlight_git = true,
+            },
+            view = {
+                width = {
+                    max = 40,
+                },
+            },
+        },
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		configs = function()
-			require("custom.configs.config")
-		end,
-	},
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        requires = {
+            "neovim/nvim-lspconfig",
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "yamlls",
+                    "omnisharp",
+                    -- lua stuff
+                    "lua-language-server",
+                    "stylua",
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("plugins.configs.lspconfig")
-			require("custom.configs.lspconfig")
-		end,
-	},
+                    -- web dev
+                    "css-lsp",
+                    "html-lsp",
+                    "svelte-language-server",
+                    "typescript-language-server",
+                    "prisma-language-server",
+                    "json-lsp",
 
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		requires = {
-			"neovim/nvim-lspconfig",
-			"williamboman/mason.nvim",
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"yamlls",
-					"omnisharp",
-					-- lua stuff
-					"lua-language-server",
-					"stylua",
+                    -- shell
+                    "shfmt",
+                    "shellcheck",
+                    --
+                    "pyright",
+                    "gopls",
+                },
+            })
+        end,
+    },
 
-					-- web dev
-					"css-lsp",
-					"html-lsp",
-					"svelte-language-server",
-					"typescript-language-server",
-					"prisma-language-server",
-					"json-lsp",
-
-					-- shell
-					"shfmt",
-					"shellcheck",
-					--
-					"pyright",
-					"gopls",
-				},
-			})
-		end,
-	},
-
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		after = "nvim-lspconfig",
-		config = function()
-			require("custom.configs.null-ls")
-		end,
-	},
-
-	{
-		"nolleh/jaq-nvim",
-		config = function()
-			require("custom.configs.config")
-		end,
-	},
-	{ "vimwiki/vimwiki" },
-	{ "NLKNguyen/pipe.vim" },
-	{ "nolleh/pipe-mysql.vim" },
-	{ "wfxr/minimap.vim" },
-	-- ["chrisbra/Colorizer"] = {},
-	-- {{ do yarn install
-	{ "iamcco/markdown-preview.nvim" },
-	-- }}
+    { "vimwiki/vimwiki", event = { "BufReadPre", "BufNewFile" } },
+    { "NLKNguyen/pipe.vim", cmd = { "PipeUse", "PipeToFile" } },
+    { "nolleh/pipe-mysql.vim", event = { "BufReadPre", "BufNewFile" } },
+    { "wfxr/minimap.vim", cmd = { "MinimapToggle" } },
+    -- ["chrisbra/Colorizer"] = {},
+    -- {{ do yarn install
+    { "iamcco/markdown-preview.nvim", cmd = { "MarkdownPreviewToggle" } },
+    -- }}
 }
