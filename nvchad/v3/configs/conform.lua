3bc44f11cf7976d93b4c5adfd8ff86d34a8e2a3d
nolleh <nolleh7707@gmail.com>
2025-03-02 20:30:53 +0900

    chore: conform

---
+++ nvchad/v3/configs/conform.lua
@@ -0,0 +1,18 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua"},
+    css = { "prettier"},
+    html = { "prettier"},
+    typescript = {"prettier"},
+    javascript = {"prettier"},
+    svelte = {"prettier"},
+    json = { "prettier"},
+    markdown = {"prettier"},
+
+    cpp = {"clang-format"},
+    go = { "gofmt"},
+    cs = {"csharpier"},
+  }
+}
+
+return options
