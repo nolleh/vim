e4ed61c7dfd8b39bac4af1327120a2478b1dc30c
nolleh <nolleh7707@gmail.com>
2025-03-03 14:27:52 +0900

    chore: commet out shadowing mapping repeating move

--- nvchad/v3/lua/mappings.lua
+++ nvchad/v3/lua/mappings.lua
@@ -5,7 +5,7 @@ require("custom.mappings")
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
