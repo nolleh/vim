f82fbe1c45becd02f14e303b7a7eb44d096aa96f
nolleh <nolleh7707@gmail.com>
2025-05-02 11:24:47 +0900

    chore: lazygit.nvim

--- nvchad/v3/lua/custom/plugins.lua
+++ nvchad/v3/lua/custom/plugins.lua
@@ -515,6 +515,25 @@ local plugins = {
     cmd = { "SQLSnapSelectDB", "SQLSnapExecute", "SQLSnapExecuteBuf" },
     ft = { "sql" },
   },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("lazygit")
+    end,
+  },
 }
 
 return plugins
