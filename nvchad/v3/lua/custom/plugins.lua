561860d918073ef5d18494679400472bf8d855a2
nolleh <nolleh7707@gmail.com>
2025-05-02 11:35:55 +0900

    chore(vim): laygit load keybinding

--- nvchad/v3/lua/custom/plugins.lua
+++ nvchad/v3/lua/custom/plugins.lua
@@ -530,6 +530,9 @@ local plugins = {
       "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim",
     },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
     config = function()
       require("telescope").load_extension("lazygit")
     end,
